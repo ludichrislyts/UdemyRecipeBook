be89fde72ac07ee1ba203a2df156d733e88489e5
E. M <ludichrislyts@gmail.com>
2017-10-07 14:58:11 -0700

    preserve user token on page refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { AuthService } from "./auth/auth.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+  constructor(private auth: AuthService) {}
   ngOnInit() {
     firebase.initializeApp({
       apiKey: "AIzaSyAJwOGZwgrQ_sn0fsZueA-mxGT6fugBKL0",
